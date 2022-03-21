f05184bf09b003d6e8c730e41aef853fe2f11f79
pjsdevcom <mail@pjsdev.com>
2022-03-21 20:31:19 +0000

    Initiate Allocate Order - closes #4

---
+++ src/main/java/com/pjsdev/brewery/model/events/AllocateOrderRequest.java
@@ -0,0 +1,15 @@
+package com.pjsdev.brewery.model.events;
+
+import com.pjsdev.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateOrderRequest {
+    private BeerOrderDto beerOrder;
+}
